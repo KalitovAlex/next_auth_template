b81dff42b614ed14497cded3b8620bcdc781be17
rsgamertv <Wersuser2@gmail.com>
2024-11-05 10:57:35 +0300

    #fix ,decomposited bg to other file

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,4 +1,5 @@
 import { AuthMetadata } from "@/shared/enums/metadata";
+import { AuthBackground } from "@/shared/ui/background/auth-background";
 import { AuthFormWidget } from "@/widgets/auth";
 import { Metadata } from "next";
 
@@ -10,9 +11,7 @@ export const metadata: Metadata = {
 export default function AuthPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-white dark:bg-[#0A0A0B] relative overflow-hidden">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-[#00C1A0] dark:bg-[#007A6D] rounded-full blur-[128px] opacity-[0.15]" />
-      <div className="absolute top-20 right-20 w-[350px] h-[350px] bg-[#6C7281] dark:bg-[#4B545D] rounded-full blur-[128px] opacity-[0.1]" />
-      <div className="absolute bottom-20 left-20 w-[350px] h-[350px] bg-[#6C7281] dark:bg-[#4B545D] rounded-full blur-[128px] opacity-[0.1]" />
+      <AuthBackground />
 
       <AuthFormWidget />
     </div>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import { RegisterWidget } from "@/widgets/register/ui/register-widget";
 import { RegisterMetadata } from "@/shared/enums/metadata";
+import { AuthBackground } from "@/shared/ui/background/auth-background";
 
 export const metadata: Metadata = {
   title: RegisterMetadata.TITLE,
@@ -10,9 +11,7 @@ export const metadata: Metadata = {
 export default function RegisterPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-background relative overflow-hidden">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-[#00C1A0] dark:bg-[#007A6D] rounded-full blur-[128px] opacity-[0.15]" />
-      <div className="absolute top-20 right-20 w-[350px] h-[350px] bg-[#6C7281] dark:bg-[#4B545D] rounded-full blur-[128px] opacity-[0.1]" />
-      <div className="absolute bottom-20 left-20 w-[350px] h-[350px] bg-[#6C7281] dark:bg-[#4B545D] rounded-full blur-[128px] opacity-[0.1]" />
+      <AuthBackground />
 
       <RegisterWidget />
     </div>
